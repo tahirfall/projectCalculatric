ef0fef3423b5a478cf66637698d4662ad887b759
tahirfall <falltahir20@gmail.com>
2024-02-20 11:10:48 +0000

    update de CalculatriceApplication

--- src/main/java/com/example/Calculatrice/CalculatriceApplication.java
+++ src/main/java/com/example/Calculatrice/CalculatriceApplication.java
@@ -8,13 +8,17 @@ import com.example.Calculatrice.services.Calculatrice;
 @SpringBootApplication
 public class CalculatriceApplication {
 	
+	static int a = 4;
+	static int b = 12;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatriceApplication.class, args);
 		
-		Calculatrice cal= new Calculatrice();
-		System.out.println("4 + 12 = " +cal.add(4, 12));
+		Calculatrice cal = new Calculatrice();
+		System.out.println(a + " + " + b + " = " + cal.add(a, b));
+		System.out.println(b + " - " + a + " = " + cal.diff(a, b));
+		System.out.println(a + " * " + b + " = " + cal.mult(a, b));
 	}
-	
 	
 	
 
